5c77d503a5db5e7fed46c60f63ed139a6cac4f21
Qi shengjie <59527207+qishengjie9@users.noreply.github.com>
2024-11-18 17:42:46 +0800

    Update eigenvalue_calculate.m

--- eigenvalue_calculate.m
+++ eigenvalue_calculate.m
@@ -1,7 +1,6 @@
 function eigenvalue = eigenvalue_calculate(data)
 % normalized data
 data_normalized = data/norm(data);
-% 计算协方差矩阵
 covariance_data = cov(data_normalized');
 eigen = eig(covariance_data);
 eigenvalue = sort(eigen,'descend');
