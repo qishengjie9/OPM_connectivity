914f96409bcf0912e4482af01d7bce051186c14b
Qi shengjie <59527207+qishengjie9@users.noreply.github.com>
2024-11-18 17:42:08 +0800

    Update eigenvalue_comp.m

--- eigenvalue_comp.m
+++ eigenvalue_comp.m
@@ -1,6 +1,6 @@
 clear all
 clc
-addpath('H:\matlab toolbox\fieldtrip-master_29092019\fieldtrip-master'); % 替换为你的 FieldTrip 安装路径
+addpath('H:\matlab toolbox\fieldtrip-master_29092019\fieldtrip-master'); 
 % addpath E:\matlab_toolbox\brainstorm3\
 % addpath E:\matlab_toolbox\fieldtrip-master_29092019\
 addpath '.\sim_time_domain';
@@ -26,7 +26,6 @@ for  s = 1:40
         load('Seeders');
         load('index_full');
         connections  = [15 16 19 20 27 28 45 46 49 50];
-        % 获取不相等的元素
         index_background=setdiff(1:66,connections);
         [~, index_noise, ~] = intersect(index_full,Seeders(index_background),'stable');       
         data_l = data_alpha(:,:,1);
@@ -43,18 +42,18 @@ for  s = 1:40
     end
 end
 cfg = [];
-cfg.dataset = 'l1-jxt1-raw.fif'; % 替换为你实际的文件名
+cfg.dataset = 'l1-jxt1-raw.fif'; 
 data_l = ft_preprocessing(cfg);
 dataMatrix_l = cat(2, data_l.trial{:});
 eigen_sub1 = eigenvalue_calculate(dataMatrix_l(1:end-1,1:15360));
 
 cfg = [];
-cfg.dataset = 'm1-jxt1-raw.fif'; % 替换为你实际的文件名
+cfg.dataset = 'm1-jxt1-raw.fif'; 
 data_m = ft_preprocessing(cfg);
 dataMatrix_m = cat(2, data_m.trial{:});
 eigen_sub2 = eigenvalue_calculate(dataMatrix_m(1:end-1,1:15360));
 cfg = [];
-cfg.dataset = 'w1-jxt1-raw.fif'; % 替换为你实际的文件名
+cfg.dataset = 'w1-jxt1-raw.fif';
 data_w = ft_preprocessing(cfg);
 dataMatrix_w = cat(2, data_w.trial{:});
 eigen_sub3 = eigenvalue_calculate(dataMatrix_w(1:end-1,1:15360));
@@ -80,19 +79,16 @@ plot([1:length(eigen_simulated{2,1})],eigen_simulated{2,1},'k--',...
      [1:length(eigen_sub3)],eigen_sub3,'g',...
     'LineWidth',1.5)
 
-set(gca, 'Box', 'off', ...                                         % 边框
-         'XGrid', 'on', 'YGrid', 'on', ...                         % 网格
-         'TickDir', 'out', 'TickLength', [.01 .01], ...            % 刻度
-         'XMinorTick', 'off', 'YMinorTick', 'off')% X坐标轴刻度标签
+set(gca, 'Box', 'off', ...                                         
+         'XGrid', 'on', 'YGrid', 'on', ...                        
+         'TickDir', 'out', 'TickLength', [.01 .01], ...            
+         'XMinorTick', 'off', 'YMinorTick', 'off')
 legend_set = {'Sim_15mm','Sim_20mm','Sim_25mm','Sim_30mm','Sim_35mm','Sim_40mm','Sub_1','Sub_2','Sub_3'};
 legend(legend_set,'Interpreter', 'latex');      
 set(gca, 'FontName', 'Times New Roman')
 %set(gca, 'FontName', 'Arial')
 set(gca, 'FontSize', 14,'FontWeight' , 'bold','LineWidth',1)
 set(gca,'xlabel')
-% 设置横坐标的范围
-xlim([0 100]);  % 设置横坐标范围从 0 到 12
-
-% 设置纵坐标的范围
-ylim([0 7e-5]);  % 设置纵坐标范围从 -1.5 到 1.5
+xlim([0 100]);  
+ylim([0 7e-5]); 
 disp(eigenvalues);
